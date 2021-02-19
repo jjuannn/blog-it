cfc04175146b814715d297a2df2230faf3a16742
juan <juancraft12yt@gmail.com>
2021-02-18 23:32:03 -0300

    Update import of resourses and global styles
    Imported new fonts and fontawesome cdn

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import NavigationBar from "./components/nav/nav";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import LandingPage from "./pages/landing/landing";
+import PageFooter from "./components/footer/footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <NavigationBar />
+        <Switch>
+          <Route path="/" exact>
+            <LandingPage />
+          </Route>
+          <Route path="/prueba" exact>
+            <p>Probando</p>
+          </Route>
+        </Switch>
+        <PageFooter />
+      </Router>
+    </>
   );
 }
 
