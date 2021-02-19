0018ebe93069f6298e9a8843879d55f6352a1a13
juan <juancraft12yt@gmail.com>
2021-02-18 23:30:28 -0300

    Created landing page view

---
+++ src/pages/landing/landing.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./landing.css";
+import iphoneImg from "../../assets/iphone-img.png";
+
+export default function LandingPage() {
+  return (
+    <section>
+      <div className="landing-container">
+        <strong className="title-text">
+          Share your thoughts with everyone...
+        </strong>
+        <p className="subtitle-paragraph">
+          ... wherever you want, whenever you want
+        </p>
+        <div className="app-mobile-presentation">
+          <img
+            src={iphoneImg}
+            alt="iphone displaying the app mobile version"
+            className="img"
+          />
+          <div className="buttons-container">
+            <button className="app-mobile-buttons">
+              <i style={{ marginRight: "6px" }} className="fab fa-apple"></i>
+              for iOS{" "}
+            </button>
+            <button className="app-mobile-buttons">
+              <i style={{ marginRight: "6px" }} className="fab fa-android"></i>
+              for Android{" "}
+            </button>
+            <button className="app-mobile-buttons">
+              <i style={{ marginRight: "6px" }} className="fab fa-chrome"></i>
+              for browsers
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
