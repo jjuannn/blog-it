de13f5d3d8d4abd71ec0e4b2e809d1c077421859
juan <juancraft12yt@gmail.com>
2021-03-02 21:50:04 -0300

    Implemented userContextProvider in app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import React from "react"
 import "./styles/breakpoints.css";
 import NavigationBar from "./components/nav/nav";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
@@ -6,24 +7,28 @@ import LandingPage from "./pages/landing/landing";
 import PageFooter from "./components/footer/footer";
 import AboutPage from "./pages/about/about";
 import UserForm from "./pages/form/userForm"
+import UserContextProvider from "./context/userContext"
+
 function App() {
   return (
     <>
-      <Router>
-        <NavigationBar />
-        <Switch>
-          <Route path="/" exact>
-            <LandingPage />
-          </Route>
-          <Route path="/about" exact>
-            <AboutPage />
-          </Route>
-          <Route path="/users/:action" exact>
-            <UserForm/>
-          </Route>
-        </Switch>
-        <PageFooter />
-      </Router>
+    <UserContextProvider>
+        <Router>
+          <NavigationBar />
+          <Switch>
+            <Route path="/" exact>
+              <LandingPage />
+            </Route>
+            <Route path="/about" exact>
+              <AboutPage />
+            </Route>
+            <Route path="/users/:action" exact>
+              <UserForm/>
+            </Route>
+          </Switch>
+          <PageFooter />
+        </Router>
+      </UserContextProvider>
     </>
   );
 }
