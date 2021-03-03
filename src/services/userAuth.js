47db1c9b14ff6f673592e2852ca490fa66125086
juan <juancraft12yt@gmail.com>
2021-03-03 13:27:41 -0300

    Implemented mapper when authenticating a user

--- src/services/userAuth.js
+++ src/services/userAuth.js
@@ -1,13 +1,16 @@
 import axios from "axios"
+import user from "../mappers/user"
 
-export default function userAuth(action, data){
+export default function userAuth(action, userCredentials){
+    const userAuthData = user(userCredentials)
     return axios({
         method: 'post',
         headers: { 'Content-Type': 'application/json' },
         url: `http://localhost:8080/users/${action}`,
-        data: data
-    }).then(res => { 
-        return res.data
+        data: userAuthData
+    }).then(res => {
+        const userValues = user(res.data)
+        return userValues
     }).catch(err => {
         throw new Error(err.response.data)
     })
