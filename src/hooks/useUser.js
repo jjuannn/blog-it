656652b2fbebccbbcb60d66778574889b5d5ac5c
juan <juancraft12yt@gmail.com>
2021-03-03 13:29:54 -0300

    Implemented reducer in useUser hook

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -1,22 +1,48 @@
-import {useCallback, useContext, useState} from "react"
+import {useCallback, useContext, useReducer} from "react"
 import {userContext} from "../context/userContext"
 import userAuth from "../services/userAuth"
 
+const ACTIONS = {
+    LOADING: "LOADING",
+    ERROR: "ERROR",
+    SUCCESS: "SUCCESS"
+}
+
+function authReducer(state, action){
+    const { type, payload } = action
+
+    switch(type){
+        case ACTIONS.LOADING: 
+            return {...state, loading: true, error: null};
+        case ACTIONS.ERROR: 
+            return {...state, loading: false, error: payload};
+        case ACTIONS.SUCCESS: 
+            return {...state, loading: false, error: null}
+        default: 
+            return state
+    }
+}
+
+const initialValues = {
+    loading: false, 
+    error: null
+}
+
 export default function useUser(){
     const { user, setUser } = useContext(userContext)
-    const [ authState, setState ] = useState({loading: false, error: null})
+    const [ authState, dispatch] = useReducer(authReducer, initialValues)
 
-    const login = useCallback((action, data) => {
-        setState({loading: true, error: null})
+    const login = (action, data) => {
+        dispatch({type: ACTIONS.LOADING})
         userAuth(action, data)
         .then((res) => {
-            setState({loading: false, error: null})
+            dispatch({type: ACTIONS.SUCCESS })
             setUser(res)
             window.sessionStorage.setItem("user", JSON.stringify(res))
         }).catch(err => {
-            setState({loading: false, error: err})
+            dispatch({type: ACTIONS.ERROR, payload: err})
         })
-    }, [setUser])
+    }
 
     const logout = useCallback(() => {
         window.sessionStorage.removeItem("user")
