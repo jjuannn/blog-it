a9b57a004366317c57b17c615efaa8bac3273c66
juan <juancraft12yt@gmail.com>
2021-03-03 22:31:34 -0300

    Implement server-side validations for empty credentials when register or login

--- api/module/user/controller/controller.js
+++ api/module/user/controller/controller.js
@@ -22,7 +22,7 @@ class UserController extends AbstractController {
           if(err instanceof UsernameAlreadyTakenError){
             res.status(401).send('Username already taken!')
           }
-          res.status(401).send(err)
+          res.status(401).send("Something went wrong. Try again!")
         }
         if(user){ 
           res.status(200).send(user)
@@ -41,7 +41,7 @@ class UserController extends AbstractController {
           if(err instanceof UserNotFoundError){
             res.status(401).send('Username not exist!')
           }
-          res.status(401).send(err)
+          res.status(401).send("Something went wrong. Try again!")
         }
         if(user){ 
           res.status(200).send(user)
--- api/module/user/repository/repository.js
+++ api/module/user/repository/repository.js
@@ -12,6 +12,9 @@ class UserRepository extends AbstractRepository {
   }
 
   async authUser(username, password){
+    if(username.length == 0 || username.length == 0){
+      throw new Error("Credentials cannot be empty")
+    }
     const user = await this.UserModel.findOne({where: {username}, attributes: ["id", "username", "password"]})
     if (user) {
       const matchPassword = await this.bcrypt.compare(password, user.password)
@@ -37,7 +40,9 @@ class UserRepository extends AbstractRepository {
 
   async newUser(user){
     const { username, password} = user
-
+    if(username.length == 0 || username.length == 0){
+      throw new Error("Credentials cannot be empty")
+    }
     const userExist = await this.UserModel.findOne({where: {username}, attributes: ["id", "username"]});
     if(userExist){  
       throw new UsernameAlreadyTakenError("Username already taken")
