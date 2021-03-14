5f3d50faeac26f23007c5514b4564c966db97861
juan <juancraft12yt@gmail.com>
2021-03-13 22:38:29 -0300

    Create useForm hook
    Is used when the user creates a new post

---
+++ src/hooks/useForm.js
@@ -0,0 +1,52 @@
+import {useReducer, useState} from "react"
+import createPost from "../services/createPost.js"
+
+const ACTIONS = {
+    SENDING: "SENDING",
+    FAILURE: "FAILURE",
+    SUCCESS: "SUCCESS"
+}
+
+function postReducer(state, action){
+    const {type, payload} = action
+
+    switch(type){
+        case ACTIONS.SENDING: 
+            return {...state, success: false, sending: true, error: null }
+        case ACTIONS.FAILURE:
+            return {...state, success: false, sending: false, error: payload}
+        case ACTIONS.SUCCESS:
+            return {...state, success: true, sending: false, error: null}
+        default: 
+            return state
+    }
+}
+
+const initialValues = {
+    success: false,
+    sending: false,
+    error: null
+}
+
+export default function useForm(){
+    const [formState, dispatch] = useReducer(postReducer, initialValues)
+
+    const publishPost = (postData) => {
+        dispatch({type: ACTIONS.SENDING})
+        createPost(postData)
+        .then(res => {
+            console.log(res)
+            dispatch({type: ACTIONS.SUCCESS})
+        }).catch(err => {
+            dispatch({type: ACTIONS.FAILURE, payload: err})
+        })
+    }
+    
+    return {
+        publishPost,
+        success: formState.success,
+        sending: formState.sending,
+        error: formState.error
+    }
+
+}
