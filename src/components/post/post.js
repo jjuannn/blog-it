9adfa7be60453707bf0919aedbe1cd869e1cb020
juan <juancraft12yt@gmail.com>
2021-03-14 23:31:12 -0300

    Update post component

--- src/components/post/post.js
+++ src/components/post/post.js
@@ -1,18 +1,31 @@
 import React from "react"
 import "./post.css"
-import vertical from "../../assets/1510004.jpg"
 import useUser from "../../hooks/useUser"
+import deletePost from "../../services/deletePost"
+import { useHistory } from "react-router-dom"
 
 
-export default function Post({title, text, image}){
-    const { isLogged, data } = useUser()
+export default function Post({title, text, picture, id, author_id, author_username, author_picture}){
+    const { data } = useUser()
+    const history = useHistory()
+
+    const handleDelete = async(id) => {
+        const deleted = await deletePost(id)
     
+        if(deleted){
+            history.go(0)
+        }
+    }
+
     return(
         <div className="post-card">
             <div className="post-header">
-                <img className="author-img" src={isLogged ? data.picture : vertical} alt="" />
-                {/* CAMBIAR ESTO DE ARRIBA CUANDO LOS POSTS SEAN BUSCADOS DE LA API */}
-                <strong style={{marginLeft: "10px"}}>Username Example</strong>
+                <img className="author-img" src={author_picture} alt="" />
+                <strong style={{marginLeft: "10px"}}>{author_username}</strong>
+                { data && data.id ? (data.id === author_id ? 
+                    <p onClick={() => { handleDelete(id)}} className="delete-post"><i class="fas fa-times"></i></p>
+                    : ""
+                ) : ""}
             </div>
             <div className="post-body">
                 <p className="post-title">
@@ -23,7 +36,7 @@ export default function Post({title, text, image}){
                 <p className="post-text">     
                     {text}
                 </p>
-                <img className="post-img" src={image}/>
+                {picture ? <img className="post-img" alt="" src={picture}/> : ""}
             </div>
         </div>
     )
