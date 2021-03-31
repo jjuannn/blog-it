e1c334ec58b9d5300b2a0d2f18f67cfdfc358aba
juan <juancraft12yt@gmail.com>
2021-03-30 22:42:45 -0300

    Update repository.spec.js

--- api/module/user/repository/__test__/repository.spec.js
+++ api/module/user/repository/__test__/repository.spec.js
@@ -5,11 +5,14 @@ const PostModel = require("../../../post/model/postModel")
 const { Sequelize } = require("sequelize")
 const User = require("../../entity/user")
 const UserNotFoundError = require("../error/userNotFoundError")
+const EmptyCredentialsError = require("../error/emptyCredentialsError")
+const UsernameAlreadyTakenError = require("../error/usernameAlreadyTakenError")
+const IncorrectPasswordError = require("../error/incorrectPasswordError")
 
 const bcryptMock = {
     compare: jest.fn(() => Promise.resolve()),
-    genSalt: jest.fn(() => Promise.resolve()),
-    hash: jest.fn(() => Promise.resolve())
+    genSalt: jest.fn((a) => Promise.resolve({a})),
+    hash: jest.fn((a) => Promise.resolve(`${a}`))
 }
 
 const sequelizeInstance = new Sequelize("sqlite::memory")
@@ -44,9 +47,69 @@ test("getById fails if not found a user with the id specificated", async() => {
     expect(user).toBeUndefined()
 })
 
-// test("getById returns a User if found a user with the id specificated", async() => {
-//     const user = await repository.newUser(userExample)
+test("getById returns a User if found one with the ID specificated", async() => {
+    const newUser = await repository.newUser(userExample)
+    const user = await repository.getById(1)
 
-//     const getUser = await repository.getById(1)
-//     expect(getUser).toEqual([user])
-// })
+    expect(user).toBeInstanceOf(User)
+    expect(user.id).toEqual(newUser.id)
+})
+
+test("newUser fails if the username or password length is equal to 0 ", async() => {
+    let newUser 
+    try {
+        newUser = await repository.newUser({username: "", password: ""})
+    } catch (err) {
+        expect(err).toBeInstanceOf(EmptyCredentialsError)
+    }
+    expect(newUser).toBeUndefined()
+})
+test("newUser fails if the username is already taken", async() => {
+    await repository.newUser(userExample)
+    let newUser
+    try {
+        newUser = await repository.newUser({username:"test", password:"123131"})
+    } catch (err) {
+        expect(err).toBeInstanceOf(UsernameAlreadyTakenError)
+    }
+    expect(newUser).toBeUndefined()
+})
+
+test("authUser fails if the username or password length is equal to 0", async() => {
+    let authUser 
+    try {
+        authUser = await repository.authUser("", "")
+    } catch (err) {
+        expect(err).toBeInstanceOf(EmptyCredentialsError)
+    }
+    expect(authUser).toBeUndefined()
+})
+test("authUser fails if not found a user with the username specificated", async() => {
+    let authUser 
+    try {
+        authUser = await repository.authUser("asdasda", "1231313")
+    } catch (err) {
+        expect(err).toBeInstanceOf(UserNotFoundError)
+    }
+    expect(authUser)
+})
+
+test("authUser fails if the password is incorrect", async() => {
+    bcryptMock.compare.mockImplementationOnce(() => Promise.resolve(false))
+    await repository.newUser(userExample)
+    let authUser 
+    try {
+        authUser = await repository.authUser(userExample.username, "incorrect123")
+    } catch (err) {
+        expect(err).toBeInstanceOf(IncorrectPasswordError)
+    }
+    expect(authUser).toBeUndefined()
+})
+
+test("authUser returns a User if the password is correct", async() => {
+    bcryptMock.compare.mockImplementationOnce(() => Promise.resolve(true))
+    await repository.newUser(userExample)
+    const user = await repository.authUser("test", "12345")
+
+    expect(user).toBeInstanceOf(User)
+})
